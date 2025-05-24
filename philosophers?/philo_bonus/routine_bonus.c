140a3ba6f600a02f201d13a171f6292d43a1ce40
Abdallah Khefif <abkhefif@e3r3p1.42roma.it>
2025-05-24 17:58:45 +0200

    1

---
+++ philosophers?/philo_bonus/routine_bonus.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine_bonus.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abkhefif <abkhefif@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 14:53:28 by abkhefif          #+#    #+#             */
+/*   Updated: 2025/05/18 14:44:02 by abkhefif         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers_bonus.h"
+
+void	take_forks(t_philo *philo, long long *last_meal_time)
+{
+	t_philodata	*philo_data;
+
+	philo_data = philo->philo_data;
+	if (get_time_ms() - *last_meal_time > philo_data->time_to_die)
+	{
+		sem_wait(philo_data->write_sem);
+		printf("%lld %d died\n", get_time_ms() - philo_data->start_time,
+			philo->id);
+		sem_post(philo_data->write_sem);
+		sem_post(philo_data->end_sem);
+		return ;
+	}
+	sem_wait(philo_data->forks_sem);
+	print_status(philo_data, philo->id, "has taken a fork");
+	sem_wait(philo_data->forks_sem);
+	print_status(philo_data, philo->id, "has taken a fork");
+}
+
+int	eat(t_philo *philo, long long *last_meal_time)
+{
+	t_philodata	*philo_data;
+
+	philo_data = philo->philo_data;
+	print_status(philo_data, philo->id, "is eating");
+	*last_meal_time = get_time_ms();
+	ft_sleep(philo_data->time_to_eat);
+	philo->meals_eaten++;
+	if (philo_data->must_eat > 0 && philo->meals_eaten >= philo_data->must_eat)
+	{
+		sem_post(philo_data->forks_sem);
+		sem_post(philo_data->forks_sem);
+		return (0);
+	}
+	sem_post(philo_data->forks_sem);
+	sem_post(philo_data->forks_sem);
+	return (1);
+}
+
+void	sleep_and_think(t_philo *philo, long long *last_meal_time)
+{
+	t_philodata	*philo_data;
+
+	philo_data = philo->philo_data;
+	if (get_time_ms() - *last_meal_time > philo_data->time_to_die)
+	{
+		sem_wait(philo_data->write_sem);
+		printf("%lld %d died\n", get_time_ms() - philo_data->start_time,
+			philo->id);
+		sem_post(philo_data->write_sem);
+		sem_post(philo_data->end_sem);
+		return ;
+	}
+	print_status(philo_data, philo->id, "is sleeping");
+	ft_sleep(philo_data->time_to_sleep);
+	print_status(philo_data, philo->id, "is thinking");
+}
+
+int	check_end_signal(t_philodata *philo_data)
+{
+	if (sem_trywait(philo_data->end_sem) == 0)
+	{
+		sem_post(philo_data->end_sem);
+		return (1);
+	}
+	return (0);
+}
+
+void	philosopher_routine_bonus(t_philo *philo)
+{
+	t_philodata	*philo_data;
+	long long	last_meal_time;
+
+	philo_data = philo->philo_data;
+	last_meal_time = philo_data->start_time;
+	if (philo->id % 2 == 0)
+		usleep(5000);
+	while (1)
+	{
+		if (check_end_signal(philo_data))
+			break ;
+		if (get_time_ms() - last_meal_time > philo_data->time_to_die)
+		{
+			sem_wait(philo_data->write_sem);
+			printf("%lld %d died\n", get_time_ms() - philo_data->start_time,
+				philo->id);
+			sem_post(philo_data->write_sem);
+			sem_post(philo_data->end_sem);
+			break ;
+		}
+		take_forks(philo, &last_meal_time);
+		if (!eat(philo, &last_meal_time))
+			break ;
+		sleep_and_think(philo, &last_meal_time);
+	}
+}
---
+++ philosophers?/philo_bonus/utils_bonus.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abkhefif <abkhefif@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 15:15:58 by abkhefif          #+#    #+#             */
+/*   Updated: 2025/05/18 14:45:50 by abkhefif         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers_bonus.h"
+
+int	is_valid_number(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t			i;
+	unsigned char	*ptr;
+
+	i = 0;
+	ptr = (unsigned char *)b;
+	while (i < len)
+		ptr[i++] = (unsigned char)c;
+	return (b);
+}
+
+int	check_simulation_status_bonus(t_philodata *data)
+{
+	int	status;
+
+	if (sem_trywait(data->end_sem) == 0)
+	{
+		sem_post(data->end_sem);
+		return (0);
+	}
+	sem_wait(data->status_sem);
+	status = data->simulation_running;
+	sem_post(data->status_sem);
+	return (status);
+}
+
+void	cleanup_resources(t_philodata *philo_data)
+{
+	sem_close(philo_data->simulation_sem);
+	sem_close(philo_data->forks_sem);
+	sem_close(philo_data->write_sem);
+	sem_close(philo_data->status_sem);
+	sem_close(philo_data->meal_sem);
+	sem_unlink("/simulation");
+	sem_unlink("/forks");
+	sem_unlink("/write");
+	sem_unlink("/status");
+	sem_unlink("/meal");
+	if (philo_data->forks)
+		free(philo_data->forks);
+	free(philo_data->philosophers);
+	free(philo_data);
+}
